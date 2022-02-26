101af6cd984424e3cfd94fbcce1de48a475fddd7
osanlan <36444262+osanlan@users.noreply.github.com>
2022-02-26 17:33:53 +0200

    songs2

--- src/routes/songs.svelte
+++ src/routes/songs.svelte
@@ -1,1 +1,16 @@
-Täällä biisejä
+<script>
+  import songs from '../assets/songs.json';
+</script>
+
+<svelte:head>
+  <title>Biisit - HybridiSpeksit</title>
+</svelte:head>
+
+<section class="wrap">
+  {#each songs as song}
+    <div>{song.name}</div>
+    {#each song.verses as verse}
+      <div>{verse}</div>
+    {/each}
+  {/each}
+</section>
