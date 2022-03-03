906e0ccaa5def0a57b4016aef6f255c7820e530c
osanlan <36444262+osanlan@users.noreply.github.com>
2022-03-04 00:19:29 +0200

    songsstuff

--- src/routes/songs.svelte
+++ src/routes/songs.svelte
@@ -1,5 +1,8 @@
 <script>
   import songs from '../assets/songs.json';
+
+  let selectedSong = songs[0];
+  let selectedYear;
 </script>
 
 <svelte:head>
@@ -7,10 +10,68 @@
 </svelte:head>
 
 <section class="wrap">
-  {#each songs as song}
-    <div>{song.name}</div>
-    {#each song.verses as verse}
-      <div>{verse}</div>
-    {/each}
-  {/each}
+  <form>
+    <select bind:value={selectedYear}>
+      <option value="9999" selected="selected">Meidän speksi</option>
+      <option value="2022">2022 Inferno</option>
+      <option value="2021">2021 Inferno - the beginning</option>
+      <option value="2020">2020 Avaruusristeily 2101</option>
+      <option value="2019">2019 Viimeinen Lohikäärmeisku</option>
+      <option value="2018">2018 Älä Ammu Ohi</option>
+      <option value="2017">2017 Kruunun Kohtalo</option>
+      <option value="2016">2016 Bratva Kontra</option>
+      <option value="2015">2015 H.A.L.I.</option>
+    </select>
+
+    <select bind:value={selectedSong}>
+      <option value="">Valitse biisi</option>
+      {#each songs as song}
+        {#if song.speksi === selectedYear}
+          <option value={song}>{song.title}</option>
+        {/if}
+      {/each}
+    </select>
+  </form>
+  <!-- {selectedSong} -->
+  {#if selectedSong}
+    <div class="song" id={selectedSong.id}>
+      <div class="name">{selectedSong.title}</div>
+      <div class="origin">{selectedSong.origin}</div>
+      {#each selectedSong.lines as line}
+        <p>{line}</p>
+      {/each}
+    </div>
+  {/if}
 </section>
+
+<style lang="scss">
+  .song {
+    margin: 20px;
+    .name {
+      // margin: 10px;
+      font-size: 2rem;
+    }
+    .speksi {
+      margin: 10px;
+      padding-left: 5px;
+      font-size: 1.5rem;
+    }
+    .origin {
+      margin: 10px;
+      padding-left: 5px;
+      font-size: 1.5rem;
+      font-style: italic;
+    }
+
+    // .verse {
+    //   display: flex;
+    //   flex-direction: column;
+    //   margin: 10px;
+    p {
+      margin: 3px;
+    }
+    // }
+    display: flex;
+    flex-direction: column;
+  }
+</style>
