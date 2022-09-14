f846dececca04561be5a1d24487634ebc37763d0
osanlan <osanlan@utu.fi>
2022-09-14 16:42:05 +0300

    rekry link timed

--- src/routes/rekry.svelte
+++ src/routes/rekry.svelte
@@ -1,3 +1,11 @@
+<script>
+import { onMount } from "svelte";
+
+let time;
+    onMount(async () => {
+        time = Date.now();
+    })  
+</script>
 <svelte:head>
   <title>Rekry - HybridiSpeksi</title>
 </svelte:head>
@@ -11,10 +19,18 @@
         <p>Tule rohkeasti mukaan tekemään yhtä Turun suurimmista spekseistä!</p>
         <p>HybridiSpeksin rekrytilaisuus on <strong>ke 14.09.2022 klo 17 Quantumin auditoriossa</strong>, nähdään siellä!</p>
         <p>Nähdään myös etänä <a href="https://utu.zoom.us/j/69656087568">zoomissa!</a></p>
+        {#if time > 1663164000000}
+        <p><a class="link" href="https://forms.gle/k5dy8NHvKFyyXNBEA">Rekrylomake</a></p><br><br>
+        {/if}
     </div>
 </section>
 <style lang="scss">
     @use '../main.scss';
+    .link {
+        padding: 10px;
+        border: brown 2px solid;
+        border-radius: 50%;
+    }
     .banner-text {
         max-width: 60vw;
         min-height: 80vh;
